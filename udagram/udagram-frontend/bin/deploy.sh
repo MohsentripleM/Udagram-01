6509f54379039cfcf6488861f42c94324740f4bc
Mohsen <eng.mohsen18@gmail.com>
2022-07-03 03:26:42 +0200

    first commit

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://bucket-fe/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://bucket-fe/
+aws s3 cp --recursive --acl public-read ./www $S3_BUCKET
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html $S3_BUCKET
